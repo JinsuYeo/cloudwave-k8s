93eedb065441059e1320ff95dbfd0fb149bfc623
Jinsu <jsyeo97@naver.com>
2024-07-16 11:57:01 +0900

    Pods, Label, Annotation, Namespace, Liveness probes, Replication Controller, ReplicaSet 학습

---
+++ practice/useful_command.sh
@@ -0,0 +1,20 @@
+# get credential of EKS
+aws eks update-kubeconfig --region ap-northeast-2 --name cwave
+
+# test aws cli
+aws s3 ls
+aws sts get-caller-identity
+
+# aws default profile
+aws configure --profile cwave
+export AWS_PROFILE=cwave
+
+# kubectl rename context-name
+kubectl config rename-context old-name new-name
+
+# create docker volume
+# docker volume create --name local_code --opt device=C:\Users\KHP59\cwave-kkk --opt o=bind
+docker volume create --name local_code --opt type=none --opt device=C:\Users\KHP59\cwave-kkk/local_code --opt o=bind
+docker volume create --name remote_code --opt type=none --opt device=C:\Users\KHP59\cwave-kkk/remote_code --opt o=bind
+docker volume create --name config --opt type=none  --opt device=C:\Users\KHP59\cwave-kkk/config --opt o=bind
+
